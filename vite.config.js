02d195e1c8bc368aae9d56931be84ab6a8cd8857
Juliana R Fernandes <littlejuh@live.it>
2025-11-11 22:14:45 -0300

    allow host

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: parseInt(process.env.PORT) || 4173,
-  }
+  },
+
+ preview: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 4173,
+    allowedHosts: ['products-react-2la2.onrender.com'],
+}
 })
+
